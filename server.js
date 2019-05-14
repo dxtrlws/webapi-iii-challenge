36718310fde7e405b80bdbc4a5fdf3078e00148c
Dexter Lewis <dexter.lewis@gmail.com>
2019-05-14 19:15:20 -0400

    got server up and running

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-const express = 'express';
-
+const express = require('express');
+const usersRouter = require('./users/userRouter')
 const server = express();
 
+// Routes
+server.use('/api/users', usersRouter)
+
+// Middleware
+server.use(logger);
+
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
-
-};
+  console.log(`${req.method} ${req.path} ${Date.now()}`);
+  next();
+}
 
 module.exports = server;
