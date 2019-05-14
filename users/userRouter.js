cc2569e54d2d3d0d4dbf8a63862f42f13b1ff9bb
Dexter Lewis <dexter.lewis@gmail.com>
2019-05-14 19:15:33 -0400

    able to validate id middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,50 @@
-const express = 'express';
+const express = require('express');
+const db = require('./userDb');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/', (req, res) => {});
 
+router.post('/:id/posts', (req, res) => {});
+
+router.get('/', async (req, res) => {
+  try {
+    const users = await db.get();
+    res.status(200).json(users);
+  } catch (err) {
+    res.send(500).json({ message: 'Unable to get users' });
+  }
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.get('/:id', validateUserId, async (req, res) => {
+  res.status(200).json(req.user);
 });
 
-router.get('/', (req, res) => {
+router.get('/:id/posts', (req, res) => {});
 
-});
+router.delete('/:id', (req, res) => {});
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put('/:id', (req, res) => {});
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+//   console.log(user);
+  try {
+    const user = await db.getById(req.params.id);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(400).json({ message: 'invalid user id' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: 'failed to process request' });
+  }
+}
 
-};
+function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+function validatePost(req, res, next) {}
 
 module.exports = router;
